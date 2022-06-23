a2be593250b0d9bd2b4d558848564466eea09aa5
ryanbleach <40777867+ryanbleach@users.noreply.github.com>
2022-06-23 17:23:50 +0000

    Update file.php

--- file.php
+++ file.php
@@ -1,10 +1,10 @@
 {"rage":"online",
 "semi":"online",
-"loader":"online",
+"loader":"offline",
 "primrage":"online",
 "versionsemi":"1.0",
 "versionprim":"1.0",
 "versionrage":"1.9.2",
 "versionragebeta":"2.0",
-"versionloader":"1.3"}
+"versionloader":"1.9"}
 
